6e4ebcfb240f782151c9a6168f27e56e9ff4377a
josiascardosodias-glitch <josiascardosodias@gmail.com>
2026-02-09 16:55:04 -0300

    primeiro comit

---
+++ script.js
@@ -0,0 +1,13 @@
+function togglemode() {
+  const html = document.documentElement
+
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./image/avatar-light.png")
+  } else {
+    img.setAttribute("src", "./image/avatar.png")
+  }
+}
